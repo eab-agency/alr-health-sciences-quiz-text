0310a1c37b2dff74482b7028fd911e7bc22e6468
Bob_Smith <bobsmith@eab.com>
2023-03-02 23:40:12 -0500

    add html and meta attributes to try to stop chrome from translating page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,16 @@ import Quiz from '@/components/Quiz';
 
 export default function Home() {
     return (
-        <div>
+        <>
             <Head>
+                <html
+                    lang="en"
+                    xmlLang="en"
+                    xmlns="http://www.w3.org/1999/xhtml"
+                />
+                <meta charset="UTF-8" />
+                <meta name="google" content="notranslate" />
+                <meta httpEquiv="Content-Language" content="en" />
                 <title>Health Science Quiz</title>
                 <meta name="description" content="Cappex Health Science Quiz" />
                 <meta
@@ -16,6 +24,6 @@ export default function Home() {
             <main>
                 <Quiz />
             </main>
-        </div>
+        </>
     );
 }
