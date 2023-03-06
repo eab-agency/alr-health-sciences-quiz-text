03718425ea00586e31f3461197244776121a9d0d
Bob_Smith <bobsmith@eab.com>
2023-03-06 11:21:21 -0500

    add ip address test

--- src/pages/practitioner.jsx
+++ src/pages/practitioner.jsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const PractitionerPage = () => <div>PractitionerPage</div>;
+const PractitionerPage = () => {
+    const [userLocation, setUserLocation] = useState(null);
+    const apiURL = 'https://ipgeolocation.abstractapi.com/v1/';
+
+    // const getUserLocationFromAPI = async () => {
+    //     axios
+    //         .get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${apiKey}`)
+    //         .then((response) => {
+    //             console.log(response.data);
+    //             setUserLocation(response.data);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // };
+
+    React.useEffect(() => {
+        const getData = setTimeout(() => {
+            axios
+                .get(
+                    `https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.ABSTRACT_API_KEY}`
+                )
+                .then((response) => {
+                    console.log(response.data);
+                    setUserLocation(response.data);
+                });
+        }, 2000);
+
+        return () => clearTimeout(getData);
+    }, []);
+
+    return (
+        <div>
+            Looks like you are a Practitioner from {userLocation?.city},{' '}
+            {userLocation?.region}!
+            <div />
+        </div>
+    );
+};
 
 export default PractitionerPage;
