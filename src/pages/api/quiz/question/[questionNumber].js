f2d37bae8fa114c3368f7951bd3e66b179f9525d
Bob_Smith <bobsmith@eab.com>
2023-03-10 10:31:05 -0500

    hooked up question to api

---
+++ src/pages/api/quiz/question/[questionNumber].js
@@ -0,0 +1,26 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export default async function handler(req, res) {
+    // Find the absolute path of the data directory
+    const dataDirectory = path.join(process.cwd(), 'data');
+    // Read the json data file data.json
+    const fileContents = await fs.readFile(
+        `${dataDirectory}/quiz.json`,
+        'utf8'
+    );
+    // Parse the JSON string into an object
+    const data = JSON.parse(fileContents);
+    // Extract the question number from the request query parameters
+    const { questionNumber } = req.query;
+    // Find the index of the question with the specified number in the questions array
+    const questionIndex = data.questions[questionNumber];
+    // If the question is found, return its index in json format. Otherwise, return a 404 error
+    if (questionIndex !== -1) {
+        res.status(200).json(questionIndex);
+    } else {
+        res.status(404).json({
+            message: `Question ${questionNumber} not found`,
+        });
+    }
+}
---
+++ src/pages/swr.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useRequest } from '@/hooks/useRequest';
+
+const SWR = () => {
+    const { data, error } = useRequest('/quiz/question', 1);
+    console.log('🚀 ~ file: swr.js:6 ~ SWR ~ data:', data);
+    // Handle the error state
+    if (error) return <div>Failed to load</div>;
+    // Handle the loading state
+    if (!data) return <div>Loading...</div>;
+    return <div>SWR</div>;
+};
+
+export default SWR;
