87c78303ba703892fbe9339c838582c9232ed0aa
Bob_Smith <bobsmith@eab.com>
2023-03-12 21:43:35 -0400

    move access token to external function

--- src/pages/api/acs/forms/[id].js
+++ src/pages/api/acs/forms/[id].js
@@ -1,34 +1,9 @@
-const tokenEndpoint = 'https://go.cappex-health.com/oauth/v2/token';
-const clientId = process.env.ACS_PUBLIC_KEY;
-const clientSecret = process.env.ACS_PRIVATE_KEY;
-let accessToken = null;
-let tokenExpiration = 0;
-
-async function getToken() {
-    if (!accessToken || Date.now() >= tokenExpiration) {
-        const tokenResponse = await fetch(tokenEndpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                Authorization: `Basic ${Buffer.from(
-                    `${clientId}:${clientSecret}`
-                ).toString('base64')}`,
-            },
-            body: `grant_type=client_credentials`,
-        });
-
-        const tokenData = await tokenResponse.json();
-        accessToken = tokenData.access_token;
-        tokenExpiration = Date.now() + tokenData.expires_in * 1000;
-    }
-
-    return accessToken;
-}
+import { getAccessToken } from '@/lib/token-utils';
 
 export default async (req, res) => {
     const { id } = req.query;
 
-    const token = await getToken();
+    const token = await getAccessToken();
 
     const response = await fetch(
         `https://go.cappex-health.com/api/forms/${id}`,
