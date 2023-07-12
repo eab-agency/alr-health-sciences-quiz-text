46c3cd14919e9c8e0027eca3c012eb57293802a0
Bob_Smith <reversetype@gmail.com>
2023-07-12 16:40:16 -0400

    🐛 fix(quiz.json): fix formatting of JSON array elements 🌟 feat(quiz.json): update information for several schools 🌟 feat(quiz.json): add new schools to the list
    The changes in the `quiz.json` file include fixing the formatting of JSON array elements to ensure consistency and readability. Additionally, the information for several schools has been updated, including the title, subtitle, description, link, link text, imageURL, logoUrl, and buttonText. Furthermore, new schools have been added to the list, including Augsburg University, Immaculata University, D’Youville University, Jacksonville University, The College of New Jersey, Montana State University, and Ithaca College. These updates and additions provide more accurate and up-to-date information about the schools in the quiz.
    
    🌈 feat(getMatchedSchool.js): add region matching functionality
    The `getMatchedSchool` function now includes region matching functionality. It checks the `state` parameter against a predefined `regionMatching` object to determine the region of the state. It then finds the first school in the `schools` array that matches the region or has a region of 'US'. If no school matches the region, it returns the first school in the array. This allows for more accurate matching of schools based on the region of the state.

--- src/helpers/getMatchedSchool.js
+++ src/helpers/getMatchedSchool.js
@@ -1,15 +1,52 @@
+const regionMatching = {
+    northeast: [
+        'ME',
+        'VT',
+        'NH',
+        'MA',
+        'RI',
+        'CT',
+        'NY',
+        'NJ',
+        'PA',
+        'MD',
+        'DE',
+    ],
+    midwest: [
+        'OH',
+        'MI',
+        'IN',
+        'IL',
+        'WI',
+        'MN',
+        'IA',
+        'MO',
+        'ND',
+        'SD',
+        'NE',
+        'KS',
+    ],
+    southwest: ['NM', 'AZ', 'CO', 'UT', 'NV', 'WY'],
+    pacific: ['CA', 'OR', 'WA', 'AK', 'HI'],
+    mountain: ['MT', 'ID', 'ND', 'SD', 'WY', 'CO', 'UT', 'NV'],
+    southeast: ['VA', 'NC', 'SC', 'GA', 'FL'],
+    southCentral: ['WV', 'KY', 'TN', 'AL', 'MS', 'AR', 'LA', 'OK', 'TX'],
+};
+
 // function that checks region_iso_code and returns a school if matched from .schools.associatedStates otherwise returns the first school from schools
 export const getMatchedSchool = (state, schools) => {
     if (schools) {
+        const region = Object.keys(regionMatching).find((region) =>
+            regionMatching[region].includes(state)
+        );
+
         const matchedSchoolInternal = schools.find(
-            (school) =>
-                school.associatedStates.includes(state) ||
-                school.associatedStates.includes('US')
+            (school) => school.region === region || school.region === 'US'
         );
 
         // If no school matches the input state, return the first school in the array
         if (!matchedSchoolInternal) {
-            return schools;
+            return schools[0];
         }
 
         const remainingSchools = schools.filter(
