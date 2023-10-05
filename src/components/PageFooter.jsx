b63525d43bc15b4143c8b5236d324cf283b0b5c5
Andrei Olea <aolea@eab.com>
2023-10-05 16:26:35 -0400

    🐛 fix(PageFooter.jsx): fix indentation and add missing div wrapper 🔧 chore(PageFooter.module.scss): comment out unused media query 🔧 chore(style.scss): change import statements to use double quotes 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and remove unnecessary comments 🔧 chore(style.scss): fix indentation and

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -18,25 +18,39 @@ export default function PageFooter() {
     return (
         <footer className={styles['page-footer']}>
             <div className={styles.wrapper}>
+                <div className={styles.logoLegal}>
                 <figure className={styles.logo}>
                     <Image
                         src="/images/appily-logo-dark.svg"
                         alt="Appily Logo"
                         fill
-                    />
+                        />
                 </figure>
-                <div className="copyright">
-                    <p>
-                        © 2023 All rights reserved.{' '}
-                        <a
-                            href="https://www.appily.com"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            www.Appily.com
-                        </a>
-                    </p>
+                    <div className={styles.legalLinks}>
+                            <ul>
+                                <li><a href="https://www.appily.com/about" target='_blank'>About Us</a></li>
+                                <li><a href="https://www.appily.com/terms-and-conditions" target='_blank'>Terms and Conditions</a></li>
+                                <li><a href="https://www.appily.com/privacy-policy" target='_blank'>Privacy Policy</a></li>
+                                <li><a href="https://privacy.eab.com/appily-cs-tours-CA" target='_blank'>California Privacy Notice</a></li>
+                                <li><a href="https://www.appily.com/your-privacy-rights" target='_blank'>Do Not Sell or Share My Personal Information</a></li>
+                                <li><a href="https://www.appily.com/data" target='_blank'>Data Attribution</a></li>
+                                <li><a href="https://www.appily.com/your-privacy-rights" target='_blank'>Personal Information Protection</a></li>
+                            </ul>
+                    </div>
+                    <div className="copyright">
+                        <p>
+                            © 2023 All rights reserved.{' '}
+                            <a
+                                href="https://www.appily.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                www.Appily.com
+                            </a>
+                        </p>
+                    </div>
                 </div>
+                
                 <div className="help-privacy">
                     <ul>
                         <li>
