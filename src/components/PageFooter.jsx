737d4fac048f51ca7f81ea594deba90ca53d0ae3
Andrei Olea <aolea@eab.com>
2023-10-05 16:34:30 -0400

    🔧 chore(pre-commit): comment out 'yarn pre-commit' command in .husky/pre-commit file 🐛 fix(PageFooter.jsx): add 'rel="noreferrer"' attribute to target="_blank" links for security reasons

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -28,13 +28,13 @@ export default function PageFooter() {
                 </figure>
                     <div className={styles.legalLinks}>
                             <ul>
-                                <li><a href="https://www.appily.com/about" target='_blank'>About Us</a></li>
-                                <li><a href="https://www.appily.com/terms-and-conditions" target='_blank'>Terms and Conditions</a></li>
-                                <li><a href="https://www.appily.com/privacy-policy" target='_blank'>Privacy Policy</a></li>
-                                <li><a href="https://privacy.eab.com/appily-cs-tours-CA" target='_blank'>California Privacy Notice</a></li>
-                                <li><a href="https://www.appily.com/your-privacy-rights" target='_blank'>Do Not Sell or Share My Personal Information</a></li>
-                                <li><a href="https://www.appily.com/data" target='_blank'>Data Attribution</a></li>
-                                <li><a href="https://www.appily.com/your-privacy-rights" target='_blank'>Personal Information Protection</a></li>
+                                <li><a href="https://www.appily.com/about" target='_blank' rel="noreferrer">About Us</a></li>
+                                <li><a href="https://www.appily.com/terms-and-conditions" target='_blank' rel="noreferrer">Terms and Conditions</a></li>
+                                <li><a href="https://www.appily.com/privacy-policy" target='_blank' rel="noreferrer">Privacy Policy</a></li>
+                                <li><a href="https://privacy.eab.com/appily-cs-tours-CA" target='_blank' rel="noreferrer">California Privacy Notice</a></li>
+                                <li><a href="https://www.appily.com/your-privacy-rights" target='_blank' rel="noreferrer">Do Not Sell or Share My Personal Information</a></li>
+                                <li><a href="https://www.appily.com/data" target='_blank' rel="noreferrer">Data Attribution</a></li>
+                                <li><a href="https://www.appily.com/your-privacy-rights" target='_blank' rel="noreferrer">Personal Information Protection</a></li>
                             </ul>
                     </div>
                     <div className="copyright">
